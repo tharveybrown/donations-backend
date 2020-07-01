170ccc87a14334f1b4a37b35865a9958dade6f92
tharveybrown <tharveybrown@gmail.com>
2020-07-01 19:15:03 -0400

    cors gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  
+  def index
+    users = User.all 
+    render json: users
+  end
+  
   def create
     User.create(user_params)
   end
+
+
  
   private
  
