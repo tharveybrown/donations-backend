829f2a68c71ef6df4541b495fdbdec0a741a6667
tharveybrown <tharveybrown@gmail.com>
2020-07-02 09:57:02 -0400

    add login routes, helpers, cors config

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   has_many :expenses
   has_many :entities, through: :expenses
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  def full_name
+    `#{self.first_name} #{self.last_name}`
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  
+  resources :users, only: [:create, :show, :index]
+  
   resources :entities
   resources :expenses
-  resources :users
-  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
