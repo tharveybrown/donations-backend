41bd112cdcecf496bf436fa452c2284588d2c7e2
tharveybrown <tharveybrown@gmail.com>
2020-07-04 10:06:02 -0400

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+
+  # protect_from_forgery with: :exception
   
   def login!
     session[:user_id] = @user.id
